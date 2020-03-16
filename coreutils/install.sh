e32b0c10edc3af2e144aba0e11399a3ca63b34b7
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2020-03-16 16:59:36 -0400

    #2

--- coreutils/install.sh
+++ coreutils/install.sh
@@ -2,7 +2,7 @@ cd $MODDIR$insloc
 for j in $(./$i --help | sed -n "/^ \[/p"); do
   if ([ ! -e "/sbin/.magisk/mirror$(echo $insloc | sed 's|^/system/vendor|/vendor|')/$j" ] && [ ! -e "$j" ] && [ "$j" != "[" ] && [ "$j" != "test" ]) || ([ "$j" == "cp" ] || [ "$j" == "mv" ]); then
     ln -sf $i $j
-    echo "$(echo $insloc | sed "s|$MODDIR||")" >> $MODDIR/.$ibinary
+    echo "$(echo $insloc | sed "s|$MODDIR||")/$j" >> $MODDIR/.$ibinary
   fi
 done
 cd $dir
